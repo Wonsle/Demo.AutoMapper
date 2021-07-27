9c21b35bc1936895c075b8ccde45fbb2ea70c22c
KunLinLee <kerberos8280@gmail.com>
2021-07-27 21:29:43 +0800

    add mapping profile add member default value add Test

--- Member.cs
+++ Member.cs
@@ -11,5 +11,13 @@ namespace Demo.AutoMapper
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+
+        public Member()
+        {
+            this.Name = "Kevin,Lin";
+            this.Phone = "0912-456-789";
+            this.Address = "台北市,大安區,羅斯福路,四段,1號";
+            //10617台北市大安區羅斯福路四段1號
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ namespace Demo.AutoMapper
     {
         private static void Main(string[] args)
         {
+            var member = new Member();
+            var _config = new MapperConfiguration(cfg => cfg.AddProfile<MemberMapping>());
+            var _mapper = _config.CreateMapper();
+            var memberVM = _mapper.Map<MemberVM>(member);
+            var member2 = _mapper.Map<Member>(memberVM);
         }
     }
 
@@ -19,8 +24,18 @@ namespace Demo.AutoMapper
     {
         public MemberMapping()
         {
-            CreateMap<Member, MemberVM>();
-            CreateMap<MemberVM, Member>();
+            CreateMap<Member, MemberVM>()
+                .ForMember(x => x.FirstName, y => y.MapFrom(o => o.Name.Split(',')[0]))
+                .ForMember(x => x.LastName, y => y.MapFrom(o => o.Name.Split(',')[1]))
+                .ForMember(x => x.Country, y => y.MapFrom(o => o.Address.Split(',')[0]))
+                .ForMember(x => x.Township, y => y.MapFrom(o => o.Address.Split(',')[1]))
+                 .ForMember(x => x.Road, y => y.MapFrom(o => o.Address.Split(',')[2]))
+                 .ForMember(x => x.Section, y => y.MapFrom(o => o.Address.Split(',')[3]))
+                 .ForMember(x => x.No, y => y.MapFrom(o => o.Address.Split(',')[4]));
+            CreateMap<MemberVM, Member>()
+                .ForMember(x => x.Name, y => y.MapFrom(o => $"{o.FirstName},{o.LastName}"))
+                .ForMember(x => x.Address
+                , y => y.MapFrom(o => $"{o.Country},{o.Township},{o.Road},{o.Section},{o.No}"));
         }
     }
 }
